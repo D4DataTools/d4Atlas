5bae3ebb04572a1c2f8ce673fdd5491c077814ac
hobbes.lucien@proton.me <hobbes.lucien@proton.me>
2026-07-15 18:57:49 -0500

    remove commented js

--- src/render.py
+++ src/render.py
@@ -73,33 +73,4 @@ def write_atlas_html(borders, markers, path):
 
 
 
-        return
-
-
-
-
-# fs.writeFileSync('docs/atlas.html', `<html>
-#   <head>
-#     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-#     <script src='https://unpkg.com/panzoom@9.4.0/dist/panzoom.min.js'></script>
-#     <link href="atlas.css" rel="stylesheet">
-#     <style id="dynamic-css"></style>
-#   </head>
-#   <body>
-#     <div class="search-input">
-#       <input type="search" id="search-input" class="form-control" placeholder="Search..." value="dungeon location">
-#     </div>
-#     <svg viewBox="-1284 -2618 3564 3564" xmlns="http://www.w3.org/2000/svg" style="width:100%;height:100%;">
-#       <g id="atlas-group" transform="matrix(3.6466190067585558 0 0 3.6466190067585558 -3030 3625)">
-#         <image href="${imageUrl}" x="-1356" y="-2724" width="3836" height="3836">
-#           <title>Sanctuary Eastern Continent</title>
-#         </image>
-#         <g transform="scale(-1, 1) rotate(45)">
-#           ${borders.join('\n          ')}
-#           ${markers.join('\n          ')}
-#         </g>
-#       </g>
-#     </svg>
-#     <script src="atlas.js"></script>
-#   </body>
-# </html>`);
+    return
