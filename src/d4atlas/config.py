4091f4187aeacc069ee2ab564be48fccfdbc914a
hobbes.lucien@proton.me <hobbes.lucien@proton.me>
2026-07-16 15:23:51 -0500

    update config in prep for loader

--- src/d4atlas/config.py
+++ src/d4atlas/config.py
@@ -1,18 +1,22 @@
 import json
 from dataclasses import dataclass
+from pathlib import Path
 from dotenv import load_dotenv, dotenv_values
 load_dotenv()
 
 @dataclass
 class D4AtlasConfig: 
     verbose: bool = False
-    DEBUG: bool = False
+    debug: bool = False
     show_timings: bool = False
     show_cache_hits: bool = False
-    LOGLEVEL: str = 'INFO'
-    IMAGE_URL: str = 'https://github.com/D4DataTools/d4Atlas/blob/main/docs/Sanctuary_Eastern_Continent_map.jpg?raw=true'
-
-DATA_ROOT = os.getenv('DATA_ROOT') or ""
+    loglevel: str = 'INFO'
+    data_root: Path | None = (
+        Path(os.getenv("DATA_ROOT"))
+        if os.getenv("DATA_ROOT")
+        else None
+    )
+    image_url: str = 'https://github.com/D4DataTools/d4Atlas/blob/main/docs/Sanctuary_Eastern_Continent_map.jpg?raw=true'
 
 SNO_GROUP_MAP = {
     "SubZone": "Subzone"
